d35c546bb683446c043f4f55587c9fbfb2949db8
BossD3struction <BossD3struction@gmail.com>
2022-02-27 21:19:12 +0100

    Initial commit

---
+++ WebAppAPI/src/main/java/cz/osu/app/WebAppApi.java
@@ -0,0 +1,11 @@
+package cz.osu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApi {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppApi.class, args);
+    }
+}
---
+++ WebAppAPI/src/test/java/cz/osu/app/WebAppApiTests.java
@@ -0,0 +1,13 @@
+package cz.osu.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApiTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
